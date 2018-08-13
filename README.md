0701e45c445d9c9d7baa97eee5e39cd63bb2440e
acsrikar279 <acsrikar@gmail.com>
2018-08-13 15:25:46 +0530

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS Fun Game
+
+### Count the number of words you can type before time expires. Have fun with your friends
---
+++ js/main.js
@@ -0,0 +1,112 @@
+window.addEventListener('load', init);
+let currentLevel = 3;
+let time = currentLevel;
+let score = 0;
+let isPlaying;
+const difficulty = document.querySelector('#difficulty');
+let choice = difficulty.options[difficulty.selectedIndex].value;
+const dummy = document.querySelector('#dummy');
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
+const words = [
+  'amazing',
+  'javascript',
+  'python',
+  'cpp',
+  'java',
+  'sql',
+  'nosql',
+  'extraordinary',
+  'fabulous',
+  'tremendous',
+  'hazardous',
+  'gluttony',
+  'scala',
+  'haskell',
+  'mongodb',
+  'android',
+  'ios',
+  'fantastic',
+  'great',
+  'speed',
+  'words',
+  'minute',
+  'quantum'
+];
+const hardwords = [
+  'amazing',
+  'javascript',
+  'python',
+  'cpp',
+  'java',
+  'sql',
+  'nosql',
+  'fabulous',
+  'tremendous',
+  'hazardous',
+  'gluttony',
+  'scala',
+  'haskell',
+  'mongodb',
+  'android',
+  'ios',
+  'fantastic',
+  'great',
+  'speed',
+  'words',
+  'minute',
+  'quantum'
+];
+function init() {
+  seconds.innerHTML = currentLevel;
+  showWord(words);
+  wordInput.addEventListener('input', startMatch);
+  setInterval(countdown, 1000);
+  setInterval(checkStatus, 50);
+  dummy.innerHTML = choice;
+}
+function startMatch() {
+  if (matchWords()) {
+    isPlaying = true;
+    time = currentLevel + 1;
+    showWord(words);
+    wordInput.value = '';
+    score++;
+  }
+  if (score === -1) {
+    scoreDisplay.innerHTML = 0;
+  } else {
+    scoreDisplay.innerHTML = score;
+  }
+}
+function matchWords() {
+  if (wordInput.value === currentWord.innerHTML) {
+    message.innerHTML = 'Correct!!!';
+    return true;
+  } else {
+    message.innerHTML = '';
+    return false;
+  }
+}
+function showWord(words) {
+  const randIndex = Math.floor(Math.random() * words.length);
+  currentWord.innerHTML = words[randIndex];
+}
+function countdown() {
+  if (time > 0) {
+    time--;
+  } else if (time === 0) {
+    isPlaying = false;
+  }
+  timeDisplay.innerHTML = time;
+}
+function checkStatus() {
+  if (!isPlaying && time === 0) {
+    message.innerHTML = 'Game Over!!!';
+    score = -1;
+  }
+}
